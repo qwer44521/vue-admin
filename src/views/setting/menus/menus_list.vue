943009c21143ea35adab16dd71f2103e33380178
qsty <1065377805@qq.com>
2020-10-24 20:12:16 +0800

    初次添加菜单列表页面

--- src/views/setting/menus/menus_list.vue
+++ src/views/setting/menus/menus_list.vue
@@ -1,12 +1,52 @@
 <template>
-<div>
- zjjjaf;j;j;
-</div>
+  <div class="app-container">
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180"
+      />
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180"
+      />
+      <el-table-column
+        prop="address"
+        label="地址"
+      />
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'MenusList'
+  name: 'MenusList',
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
+
 }
 </script>
 
